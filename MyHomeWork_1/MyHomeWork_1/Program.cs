a6834148426f6d3b936ba1a76c54fbe96392ce6f
cdlf <Vitaliidryagin@gmail.com>
2022-01-16 19:34:32 +0300

    finished
    finished

---
+++ MyHomeWork_1/MyHomeWork_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstApplication
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+
+            Console.Write("Как тебя зовут?");
+            string name = Console.ReadLine();
+
+            System.Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}");
+
+        }
+    }
+}
